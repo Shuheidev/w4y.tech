0dd773a2544c1a957e525d94af2fe21c2d60ce0f
Shuheidev <atikhonovyr@gmail.com>
2020-02-21 19:59:03 +0300

    initial source code of application

---
+++ cpgl/src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+export const HEROES: Hero[]= [
+  {id: 11, name: 'Alex'},
+  {id: 12, name: 'Roman'},
+  {id: 13, name: 'James'},
+  {id: 14, name: 'Petr'},
+  {id: 15, name: 'Boris'},
+  {id: 16, name: 'Tom'}
+];

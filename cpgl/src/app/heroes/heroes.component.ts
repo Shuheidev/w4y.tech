ecfe864302352f8030b50ba7ca5b3654f529b03a
Shuheidev <atikhonovyr@gmail.com>
2020-02-28 23:22:01 +0300

    routing

--- cpgl/src/app/heroes/heroes.component.ts
+++ cpgl/src/app/heroes/heroes.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessagesService } from '../messages.service';
-
-
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,20 +9,13 @@ import { MessagesService } from '../messages.service';
 export class HeroesComponent implements OnInit {
 
   public heroes: Hero[];
-  public selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
-    console.log('Current selected hero: ', this.selectedHero);
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
     console.warn(this.heroes);
   }
 
-  constructor(private heroService: HeroService, private messageService: MessagesService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();

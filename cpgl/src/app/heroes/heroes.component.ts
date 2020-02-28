f335eb90518055512c4ce0d81ebdb7f26ec9c1f4
Shuheidev <atikhonovyr@gmail.com>
2020-02-28 16:42:08 +0300

    messages and some logs

--- cpgl/src/app/heroes/heroes.component.ts
+++ cpgl/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessagesService } from '../messages.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -9,21 +11,24 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  public heroes = HEROES;
-  public hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
+  public heroes: Hero[];
   public selectedHero: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
+    console.log('Current selected hero: ', this.selectedHero);
   }
 
-  constructor() { }
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    console.warn(this.heroes);
+  }
+
+  constructor(private heroService: HeroService, private messageService: MessagesService) { }
 
   ngOnInit(): void {
-
+    this.getHeroes();
   }
 
 }
--- cpgl/src/app/mock-heroes.ts
+++ cpgl/src/app/mock-heroes.ts
@@ -1,9 +1,10 @@
 import { Hero } from './hero';
-export const HEROES: Hero[]= [
+export const HEROES: Hero[] = [
   {id: 11, name: 'Alex'},
   {id: 12, name: 'Roman'},
   {id: 13, name: 'James'},
   {id: 14, name: 'Petr'},
   {id: 15, name: 'Boris'},
-  {id: 16, name: 'Tom'}
+  {id: 16, name: 'Tom'},
+  {id: 17, name: 'Oleg'}
 ];
